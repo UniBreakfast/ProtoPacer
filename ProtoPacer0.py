aed1b2f8a0d8cb052a4ba26028cfc161def65fcb
UniBreakfast <unibreakfast1@gmail.com>
2017-09-15 00:53:37 +0300

    IntroClasses
    Starting with classes

--- ProtoPacer0.py
+++ ProtoPacer0.py
@@ -10,6 +10,27 @@ def it(text=None):  # Упрощённый input() для реплик Тута
     return input("    Тут:  ")
 
 
+class Endeavor:
+    def __init__(self, fName, sName, details, type):
+        self.fName = fName
+        self.sName = sName
+        self.details = details
+        self.type = type
+
+
+class Activity:
+    def __init__(self, fName, sName, amount, diff, usef):
+        self.fName=fName
+        self.sName=sName
+        self.amount=amount
+        self.diff=diff
+        self.usef=usef
+
+
+class ActiveQuest:
+    def __init__(self, fName, sName, amount, actualDiff, usef, startDate):
+        pass
+
 print("\n   Вас приветствует PACER\n")
 pp("Представьтесь пожалуйста")
 user = "Тут" # пропускаем проверку для отладки
@@ -21,11 +42,6 @@ if user == "Тут":
     if password == "Не морочь мне голову, пасер.":
         pp("Ладно, прости. Заходи, Тут. Рад тебя видеть!\n")
         try:
-            file = open('PacerData.txt', 'r+', encoding='utf-8')
-            PacerData = file.readlines()
-            print(PacerData[0])
-            file.writelines(PacerData)
-
             while True:
                 choice = input("  PACER:  Мы с тобой можем заняться твоими Стремлениями (1), Действиями (2) или Квестами (3).\nМожем поговорить о твоей Вере_В_Себя (4). Можем посмотреть Статистику (5).\nИли можешь просто Отчитаться по текущим заданиям (6). Или выйти (7).\n    Тут:  ")
                 if choice in ("1","2","3","4","5","6","7"):
@@ -37,15 +53,18 @@ if user == "Тут":
                     elif choice == 3:
                         pass
                     elif choice == 4:
-                        BBCl = int(PacerData[0][PacerData[0].find(" ВВС = ")+len(" ВВС = "):])
-                        pp("Насколько мне известно, твоя нынешняя Вера_В_Себя равняется " + str(BBCl))
+                        file = open('PacerData.txt', 'r+', encoding='utf-8')
+                        PacerData = file.readlines()
+                        BBCprev = int(PacerData[0][PacerData[0].find(" ВВС = ") + len(" ВВС = "):])
+                        pp("Насколько мне известно, твоя нынешняя Вера_В_Себя равняется " + str(BBCprev))
                         pp("Как она изменилась с прошлого раза?")
-                        BBC = BBCl + int(it())
+                        BBC = BBCprev + int(it())
                         pp("Тогда теперь твоя Вера_В_Себя составляет " + str(BBC))
                         file.seek(0)
                         file.truncate()
-                        PacerData[0]=PacerData[0].replace(str(BBCl),str(BBC))
+                        PacerData[0]=PacerData[0].replace(str(BBCprev), str(BBC))
                         file.writelines(PacerData)
+                        file.close()
                     elif choice == 5:
                         pass
                     elif choice == 6:
@@ -58,9 +77,6 @@ if user == "Тут":
                     continue
         except IndexError:
             pp("Файла нет или он пустой?")
-        finally:
-            file.close()
-
     else:
         pp("Вон отсюда, самозванец!")
 else:
